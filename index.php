3c860dd72a412285dfc4fb0fbade3ca0bc50ef13
hucaihua <1203553461@qq.com>
2019-09-04 23:53:09 +0800

    1

--- index.php
+++ index.php
@@ -1,20 +1,32 @@
 <?php
 
 //创建服务器
-$host = '0.0.0.0';
-$port = 9501;
-$service = new swoole_server($host,$port);
-$service->on('connect',function($serv,$fd){
-    echo "建立连接\n";
-    var_dump($serv);
-    var_dump($fd);
+$server = new swoole_server("127.0.0.1", 9503);
+$server->on('connect', function ($server, $fd){
+    echo "connection open: {$fd}\n";
 });
-$service->on('receive',function($serv,$rd,$fromId,$data){
-    echo "接收到数据\n";
-    var_dump($data);
+$server->on('receive', function ($server, $fd, $reactor_id, $data) {
+    $server->send($fd, "Swoole: {$data}");
+    $server->close($fd);
 });
-$service->on('close',function($service,$rd){
-   echo "服务关闭\n";
+$server->on('close', function ($server, $fd) {
+    echo "connection close: {$fd}\n";
 });
-
-$service->start();
+$server->start();
+//$host = '0.0.0.0';
+//$port = 9501;
+//$service = new swoole_server($host,$port);
+//$service->on('connect',function($serv,$fd){
+//    echo "建立连接\n";
+//    var_dump($serv);
+//    var_dump($fd);
+//});
+//$service->on('receive',function($serv,$rd,$fromId,$data){
+//    echo "接收到数据\n";
+//    var_dump($data);
+//});
+//$service->on('close',function($service,$rd){
+//   echo "服务关闭\n";
+//});
+//
+//$service->start();
