e87c89054fafbe8ea0fd7b37f298b17b9fa5d835
hucaihua <1203553461@qq.com>
2019-09-04 23:21:39 +0800

    geng

--- index.php
+++ index.php
@@ -5,7 +5,14 @@ $host = '0.0.0.0';
 $port = 9501;
 $service = new swoole_server($host,$port);
 $service->on('connect',function($serv,$fd){
-    echo '建立连接';
+    echo "建立连接\n";
     var_dump($serv);
     var_dump($fd);
+});
+$service->on('receive',function($serv,$rd,$fromId,$data){
+    echo "接收到数据\n";
+    var_dump($data);
+});
+$service->on('close',function($service,$rd){
+   echo "服务关闭\n"; 
 });
