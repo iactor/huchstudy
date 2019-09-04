b205d4daec40e23c052e8e20cb117e1fd6df852a
hucaihua <1203553461@qq.com>
2019-09-04 23:12:18 +0800

    genx1

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 
-
-phpinfo();
+//创建服务器
+$host = '0.0.0.0';
+$port = 9501;
+$service = new swoole_server($host,$port);
+$service->on('connect',function($serv,$fd){
+    echo '建立连接';
+    var_dump($serv);
+    var_dump($fd);
+});
