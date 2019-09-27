8578d7ce4a93652dafa6d42f82b440a4417fbfac
hucaihua <1203553461@qq.com>
2019-09-27 22:44:41 +0800

    0

--- udp.php
+++ udp.php
@@ -3,10 +3,10 @@
 //创建服务器
 $server = new swoole_server("0.0.0.0", 9502,SWOOLE_PROCESS,SWOOLE_SOCK_UDP);
 $server->on('packet', function ($server,$data, $fd){
-    $num = 1000;
+    $num = 100;
     for($i=0;$num >=$i;$i++){
-        $server->sendto($fd['address'],$fd['port'],$i."：$data");
-        echo $i;
+        $server->sendto($fd['address'],$fd['port'],$i."---".$data);
+        echo $i."\n";
     }
 
 });
